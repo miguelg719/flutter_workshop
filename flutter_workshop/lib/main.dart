9fb17a69f8d59bf656c9e53dc2a62dc4f1fb3351
miguelg719 <miguelg71921@gmail.com>
2020-11-06 02:38:03 -0600

    portfolio done

---
+++ flutter_workshop/lib/main.dart
@@ -0,0 +1,270 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Portfolio',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+      ),
+      home: MyHomePage(title: 'My Portfolio Home Page'),
+    );
+  }
+}
+
+class NavButton extends StatelessWidget {
+  final text;
+  final onPressed;
+  final Color color;
+
+  const NavButton(
+      {Key key,
+      @required this.text,
+      @required this.onPressed,
+      this.color = Colors.orange})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      child: Text(text),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      borderSide: BorderSide(
+        color: color,
+      ),
+      onPressed: onPressed,
+      highlightedBorderColor: color,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  List<Widget> navButtons() => [
+        NavButton(
+          text: "about",
+          onPressed: () {
+            //html.window.open("https://pawan.live", "Pk");
+          },
+        ),
+        NavButton(
+          text: "work",
+          onPressed: () {
+            //html.window.open("https://pawan.live", "Pk");
+          },
+        ),
+        NavButton(
+          text: "contact",
+          onPressed: () {
+            //html.window.open("https://pawan.live", "Pk");
+          },
+        ),
+      ];
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 10,
+        backgroundColor: Colors.black54,
+        title: Text('My Portfolio'),
+      ),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: ListView(
+            padding: const EdgeInsets.all(20),
+            children: navButtons(),
+          ),
+        ),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Container(
+                  height: 170,
+                  width: 170,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage("assets/AvatarMG.jpeg"),
+                      alignment: Alignment.center,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                    child: Text(
+                      "Hi there! My name is",
+                      textScaleFactor: 2,
+                      style: TextStyle(color: Colors.orange),
+                    ),
+                  ),
+                  Text(
+                    "Miguel\nGonzalez",
+                    textScaleFactor: 4,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Google DSC Lead for Flutter & ML\n"
+                    "I am also a senior Software Engineer and I like to play soccer",
+                    softWrap: true,
+                    textScaleFactor: 1.5,
+                    style: TextStyle(color: Colors.white70),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      RaisedButton(
+                        shape: StadiumBorder(),
+                        child: Text("Resume"),
+                        color: Colors.red,
+                        onPressed: () {
+                          //html.window.open(
+                          //    "https://google-developers.appspot.com/community/experts/directory/profile/profile-pawan_kumarGDE");
+                        },
+                        padding: EdgeInsets.all(8),
+                      ),
+                      OutlineButton(
+                        borderSide: BorderSide(
+                          color: Colors.red,
+                        ),
+                        shape: StadiumBorder(),
+                        child: Text("Say Hi!"),
+                        color: Colors.red,
+                        onPressed: () {
+                          //html.window.open("https://pawan.live", "Pk");
+                        },
+                        padding: EdgeInsets.all(8),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(height: 30),
+              Column(
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                //
+                // Invoke "debug painting" (press "p" in the console, choose the
+                // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                // to see the wireframe for each widget.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Wanna Like my Page?",
+                    textScaleFactor: 1.5,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'You have liked my page this many times:',
+                  ),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        backgroundColor: Colors.red[400],
+        tooltip: 'Increment',
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
